ddd1852349a929b346532d3717e13c6a873e7cb0
VikaVelhan <velganvika@gmail.com>
2022-12-09 01:50:56 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-const fetchUsers = async () => {
-  const baseUrl = 'https://jsonplaceholder.typicode.com';
-  const firstResponse = await fetch(`${baseUrl}/users/1`);
-  const secondResponse = await fetch(`${baseUrl}/users/2`);
-  const thirdResponse = await fetch(`${baseUrl}/users/3`);
-
-  const firstUser = await firstResponse.json();
-  const secondUser = await secondResponse.json();
-  const thirdUser = await thirdResponse.json();
-
-  console.log(firstUser, secondUser, thirdUser);
-};
-
-fetchUsers();
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
---
+++ src/js/fetchImg.js
@@ -0,0 +1,2 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '31938482-76419dff0cedda02808dc9161';
