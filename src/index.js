404a7c96928c9b05d664b8cdf57b4d2d5aea0a68
VikaVelhan <velganvika@gmail.com>
2022-12-09 00:54:02 +0200

    parcel

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const fetchUsers = async () => {
+  const baseUrl = 'https://jsonplaceholder.typicode.com';
+  const firstResponse = await fetch(`${baseUrl}/users/1`);
+  const secondResponse = await fetch(`${baseUrl}/users/2`);
+  const thirdResponse = await fetch(`${baseUrl}/users/3`);
+
+  const firstUser = await firstResponse.json();
+  const secondUser = await secondResponse.json();
+  const thirdUser = await thirdResponse.json();
+
+  console.log(firstUser, secondUser, thirdUser);
+};
+
+fetchUsers();
