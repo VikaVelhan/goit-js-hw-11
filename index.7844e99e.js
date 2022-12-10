308bfeb8836360eabc2d0f159a3a800e9612654b
VikaVelhan <106451622+VikaVelhan@users.noreply.github.com>
2022-12-10 00:55:10 +0000

    Deploying to gh-pages from @ VikaVelhan/goit-js-hw-11@1c95b06b13285c64dcde9570bf61178e4315cc82 🚀

--- index.7844e99e.js
+++
@@ -1,2 +0,0 @@
-(async()=>{const a="https://jsonplaceholder.typicode.com",s=await fetch(`${a}/users/1`),t=await fetch(`${a}/users/2`),o=await fetch(`${a}/users/3`),e=await s.json(),c=await t.json(),i=await o.json();console.log(e,c,i)})();
-//# sourceMappingURL=index.7844e99e.js.map
